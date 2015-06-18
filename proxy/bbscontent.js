d6936422371bc36d15b2749f8166832747fa9a4f
kenneth-hao <drmrontheway@gmail.com>
2015-06-18 16:46:16 +0800

    remove console log

--- proxy/bbscontent.js
+++ proxy/bbscontent.js
@@ -7,7 +7,6 @@ exports.getCountByQuery = function(query, cb) {
 
 exports.getContentsByQuery = function(query, opt, cb) {
   BbsContent.find(query, {}, opt, function(err, contents) {
-    console.log(contents.length)
     if (err) {
       return cb(err);
     }
